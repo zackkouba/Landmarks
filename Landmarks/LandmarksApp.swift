3412c421b8c521cd3c602263401cf2316d0e68e4
Zachary Kouba <zackkouba5@gmail.com>
2024-12-21 15:09:56 -0600

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Zachary Kouba on 12/21/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
